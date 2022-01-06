d3e58cb595c7b093b0ff9b5d391f128007d7e251
mmackiewicz07 <m.mackiewicz07@gmail.com>
2022-01-06 16:45:58 +0100

    setting-up-engine-and-libaries

--- remote-weather-monitoring/src/app/app-routing.module.ts
+++ remote-weather-monitoring/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from 'src/core/pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [{
+      path: '',
+      redirectTo: 'strona-glowna',
+      pathMatch: 'full'
+    },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- remote-weather-monitoring/src/app/app.module.ts
+++ remote-weather-monitoring/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/shared/shared.module';
+import { AppMaterialModule } from 'src/shared/app-material.module';
+import { CoreModule } from 'src/core/core.module';
+import { AppStoreModule } from 'src/core/app-store/app-store.module';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -12,7 +19,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    AppMaterialModule,
+    CoreModule,
+    AppStoreModule,
+    HttpClientModule,
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
